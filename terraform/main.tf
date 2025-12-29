bc0b872800b3254b9fe8880c44f18daa4a172aa2
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-12-29 12:33:44 +0530

    ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,12 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 7
+}
+
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
