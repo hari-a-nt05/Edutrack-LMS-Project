2ba0036480f91c0a2082f93ff33d3bae29a32a1d
hari-a-nt05 <hariharan.a@navattech.com>
2025-12-30 10:55:17 +0530

    ok

--- Infra-terraform/backend.tf
+++ Infra-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "lms-hari-terraform-state-2415"
+    bucket         = "lms-hari-terraform-state-2416"
     key            = "lms/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-hari"
