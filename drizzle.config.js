5866123687d6da5e9e7e3c57b7bdc2739a9877ce
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-12-30 14:57:59 +0530

    DATABASE_URL

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://ai_lms_user:ai_lms_password@localhost:5432/ai_lms",
+    url: process.env.DATABASE_URL,
   },
 });
