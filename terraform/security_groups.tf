f5db705004d1fec0ac26aab2a9438dfeae295b6a
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-12-30 16:52:35 +0530

    modified sg

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -43,4 +43,11 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.ecs_sg.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
