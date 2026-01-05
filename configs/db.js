b52f1a9a3d72067262a3b369f516885d51c2e3ee
hari-a-nt05 <hariharan.a@navattech.com>
2026-01-05 14:41:43 +0530

    db,js updated

--- configs/db.js
+++ configs/db.js
@@ -1,22 +1,23 @@
 import "server-only";
-
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const connectionString =
-  process.env.DATABASE_URL ||
-  process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING;
+let pool;
+let db;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined");
+export function getDb() {
+  if (!db) {
+    if (!process.env.DATABASE_URL) {
+      throw new Error("DATABASE_URL is not defined");
+    }
+
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    });
+
+    db = drizzle(pool);
+  }
+
+  return db;
 }
-
-const pool = new Pool({
-  connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-});
-
-export const db = drizzle(pool);
