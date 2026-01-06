c7d5d4837a7cba6cd7ea8662ef58ab1a3fabebe8
hari-a-nt05 <hariharan.a@navattech.com>
2026-01-06 11:24:08 +0530

    updated drizzle

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,13 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
+  out: "./drizzle",
   dbCredentials: {
-<<<<<<< HEAD
-    url: process.env.DATABASE_URL,
-  },
+    url: process.env.DATABASE_URL
+  }
 });
-=======
-    url: process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING || "postgresql://ai_lms_user:ai_lms_password@localhost:5432/ai_lms",
-  },
-});
->>>>>>> 1669b30742863133c51b1c572e4f0de50930b312
