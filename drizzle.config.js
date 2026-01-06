cce679e4299f9761519e601e7f641da7926b5dae
hari-a-nt05 <hariharan.a@navattech.com>
2026-01-06 12:02:40 +0530

    updated drizzle

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
-  out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL
-  }
+    url: process.env.DATABASE_URL,
+  },
 });
